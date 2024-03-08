93c70ecba3d085d59c4cd986b4e31d255f729126
Dominik Hofer <hi@dominikhofer.me>
2024-03-08 11:02:13 +0100

    Scroll functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
+import { useEffect, useRef } from 'react';
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen justify-center items-stretch flex-co p-4">
+		<main className="flex h-screen justify-center items-stretch flex-co p-4">
 			<div className="flex flex-col w-full max-w-[40rem] border rounded-lg">
-				<div className="flex-1 overflow-auto flex flex-col justify-end gap-2 p-4">
+				<ScrollContainer>
 					<Message
 						role={'bot'}
 						message={"Hey! I'm Bloq-ee. How can I help you today?"}
 					/>
 					<Message role={'user'} message={'I want a new website.'} />
-				</div>
+				</ScrollContainer>
 				<div className="flex items-center p-2 border-t">
 					<Input
 						className="border-0 flex-1"
@@ -47,6 +50,44 @@ function Message({ role, message }: { role: 'bot' | 'user'; message: string }) {
 			</div>
 			<div className="rounded-lg rounded-bl-none bg-secondary p-2 text-sm max-w-[75%]">
 				{message}
+			</div>
+		</div>
+	);
+}
+
+function ScrollContainer({ children }: { children: React.ReactNode }) {
+	const chatWindow = useRef<HTMLDivElement>(null);
+	const chatStack = useRef<HTMLDivElement>(null);
+
+	// start the container at the bottom
+	useEffect(() => {
+		const outerHeight = chatWindow.current?.clientHeight;
+		const innerHeight = chatStack.current?.clientHeight;
+
+		chatWindow.current?.scrollTo({
+			top: innerHeight && outerHeight ? innerHeight - outerHeight : 0,
+			left: 0,
+		});
+	}, []);
+
+	// scroll smoothly on change of children
+	useEffect(() => {
+		const outerHeight = chatWindow.current?.clientHeight;
+		const innerHeight = chatStack.current?.clientHeight;
+
+		chatWindow.current?.scrollTo({
+			top: innerHeight && outerHeight ? innerHeight - outerHeight : 0,
+			left: 0,
+			behavior: 'smooth',
+		});
+	}, [children]);
+
+	return (
+		<div className="overflow-y-auto flex-1 relative" ref={chatWindow}>
+			<div
+				className="absolute top-0 left-0 right-0 min-h-full p-4 flex flex-col gap-2 justify-end"
+				ref={chatStack}>
+				{children}
 			</div>
 		</div>
 	);
